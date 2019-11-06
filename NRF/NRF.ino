a6686536617c4e765db7fc54afe09211b1d1abf9
79937 <799371430@qq.com>
2019-11-06 22:22:04 +0800

    NRF test.

---
+++ NRF/NRF.ino
@@ -0,0 +1,24 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(2, 3); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
---
+++ NRF_RECV/NRF_RECV.ino
@@ -0,0 +1,27 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(2, 3); // CE, CSN
+const byte address[6] = "00001";
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
