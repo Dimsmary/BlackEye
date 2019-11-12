19db7f66833a9b8d9eff10c151d45c87c87dbee1
79937 <799371430@qq.com>
2019-11-12 20:19:30 +0800

    基本功能齐全

--- Master/Master.ino
+++ Master/Master.ino
@@ -12,8 +12,30 @@ unsigned char key_sta[2] = {0, 0};
 RF24 radio(2,3);
 // 设置NRF接口，使用默认SPI端口，2-CE / 3-CSN
 
-byte state = LOW;
-byte state1 = LOW;
+byte button1_STA = LOW;
+int buttonState1 = 0;
+int lastButtonState1 = 1;
+
+byte button2_STA = LOW;
+int buttonState2 = 0;
+int lastButtonState2 = 1;
+// 按钮中断变量
+
+
+const int  buttonPin1 = 0;   
+const int  buttonPin2 = 7;       
+// 中断输入脚
+
+
+
+const long command1 = 256;
+const long command2 = 512;
+const long command3 = 768;
+const long command4 = 1024;
+const long command5 = 1280;
+const long command6 = 1536;
+const long command7 = 1792;
+// 命令列表
 
 void setup(){
   Serial.begin(115200);
@@ -34,63 +56,104 @@ void setup(){
 
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  pinMode(12, OUTPUT);
-  // 初始化电位器读取
+// 初始化电位器读取
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+// 初始化按钮中断输入
 
-//  attachInterrupt(digitalPinToInterrupt(0), switch1, FALLING);
-//  attachInterrupt(digitalPinToInterrupt(7), switch2, LOW);
+  attachInterrupt(digitalPinToInterrupt(0), switch1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(7), switch2, CHANGE);
+// 配置中断
 }
 
 long SendData(long data){
   long gotdata;
   //定义
-  if (!radio.write( &data, sizeof(data) )){
-      return 0x100;
+  if (!radio.write(&data, sizeof(data))){
+      return 0;
       // 未发送成功      
   }else{
     if(!radio.available()){ 
-      return 0x200; 
+      return 0; 
       // 发送成功但ack为空
     }else{
       while(radio.available() ){
-        radio.read( &gotdata, sizeof(gotdata) );
+        radio.read( &gotdata, sizeof(gotdata));
       }
       return gotdata;
     }
   }
+return 0;
 }
 
 void loop(){
   int potValueX = analogRead(A0);
   int potValueY = analogRead(A1);
-  long angleValueX = map(potValueX, 0, 1023, 0, 180) + 256;
-  long angleValueY = map(potValueY, 0, 1023, 0, 180) + 512;
+  long angleValueX = map(potValueX, 0, 1023, 0, 180);
+  long angleValueY = map(potValueY, 0, 1023, 0, 180);
   // 读取电位器并与舵机角度匹配 后方为高位识别命令码
-//  radio.write(&angleValueX, sizeof(angleValueX)); 
-//  radio.write(&angleValueY, sizeof(angleValueY)); 
-  SendData(angleValueX);
-  SendData(angleValueY);
+  
+  if (angleValueX > 95){
+    SendData(command1);
+  }
+  else if(angleValueX < 85){
+    SendData(command2);
+  }
+  if (angleValueY > 95){
+    SendData(command3);
+  }
+  else if(angleValueY < 85){
+    SendData(command4);
+  }
 
-  if(digitalRead(0)){
-    digitalWrite(12, HIGH);
-  }
-  else{
-    digitalWrite(12, LOW);
-  }
-//  if (key_sta[0]){
-//    if (key_sta[1]){
-//       SendData(768);
-//    }
-//    else{
-//      SendData(1024);
-//    }
-//    key_sta[0] = 0;
-//  }
+  
+// 发送操控命令
+  
   // 按键中断检测
+  if (buttonState1 == HIGH){
+    buttonState1 = LOW;
+    Serial.println(SendData(command5));
+  }
+  if (buttonState2 == HIGH){
+    buttonState2 = LOW;
+    SendData(command6);
+  }
+  // 按键中断处理
 }
 
 void switch1(){
-      delayMicroseconds(5000);
-      state = !state;
-      digitalWrite(12, state);
+  buttonState1 = digitalRead(buttonPin1);
+  // 与上一次检测到的状态对比
+  if (buttonState1 != lastButtonState1) {
+    // 如果检测到状态改变
+    if (buttonState1 == HIGH) {
+      // 高电平为松开   
+      Serial.println("off");
+    } else {
+      // 低电平为按下
+      button1_STA = HIGH;
+      // 标志位
+      Serial.println("on");
+    }
+    //消抖
+    delay(50);
+  }
+  // 每次改变都保存状态检测改变
+  lastButtonState1 = buttonState1;
+}
+
+void switch2(){
+  buttonState2 = digitalRead(buttonPin2);
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == HIGH) { 
+      Serial.println("off");
+    } else {
+      button2_STA = HIGH;
+      // 标志位
+      Serial.println("on");
+    }
+    delay(50);
+  }
+  lastButtonState2 = buttonState2;
+  // 原理同Switch1
 }
--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -15,11 +15,32 @@ Servo myServo1;
 
 RF24 radio(2,3);
 // 设置NRF接口，使用默认SPI端口，2-CE / 3-CSN
+
+const int control1 = 7;
+const int control2 = 8;
+// MOS输出控制脚
+
+const long command1 = 256;
+const long command2 = 512;
+const long command3 = 768;
+const long command4 = 1024;
+const long command5 = 1280;
+const long command6 = 1536;
+const long command7 = 1792;
+
+// 命令列表
+
+long eye_sta[] = {0, 0, 0, 0};
+// 状态数组
+
+long servoANG[] = {900, 900};
+// 舵机角度
+
 void setup(){
   Serial.begin(115200);
   // 启用串口
   printf_begin();
-  // 启用printf
+  // 启用printf 
 
   myServo0.attach(5);
   myServo1.attach(9);
@@ -35,6 +56,10 @@ void setup(){
   radio.openReadingPipe(1,pipes[0]);
   radio.startListening();
   // 切换到发送模式
+
+  pinMode(control1, OUTPUT);
+  pinMode(control2, OUTPUT);
+  pinMode(A0, INPUT);
 }
 
 void loop(void) {
@@ -48,16 +73,44 @@ void loop(void) {
    // 识别码
    
    if(order == 1){
-    radio.writeAckPayload(pipeNo,&gotdata, sizeof(gotdata) );
-    myServo0.write(gotdata - 256);
-    
+    radio.writeAckPayload(pipeNo,&command1, sizeof(gotdata) );
+      if (servoANG[0] < 1800){
+      servoANG[0] = servoANG[0] + 1;
+    }
    }
+   
    else if(order  == 2){
-    radio.writeAckPayload(pipeNo,&gotdata, sizeof(gotdata) );
-    myServo1.write(gotdata - 512);
+    radio.writeAckPayload(pipeNo,&command2, sizeof(gotdata) );
+    if (servoANG[0] > 0){
+      servoANG[0] = servoANG[0] - 1;
+    }
+   }
+   else if(order  == 3){
+    radio.writeAckPayload(pipeNo,&command3, sizeof(gotdata) );
+    if (servoANG[1] < 1800){
+      servoANG[1] = servoANG[1] + 1;
+    }
+   }
+   else if(order  == 4){
+    radio.writeAckPayload(pipeNo,&command4, sizeof(gotdata) );
+    if (servoANG[1] > 0){
+      servoANG[1] = servoANG[1] - 1;
+    }
    }
    // 以上为舵机控制命令
-   else if(order == 3){
-    radio.writeAckPayload(pipeNo,&gotdata, sizeof(gotdata) );
+   else if(order == 5){
+    eye_sta[1] = !eye_sta[1];
+    radio.writeAckPayload(pipeNo, &command3, sizeof(eye_sta[1]) );
+    digitalWrite(control1, eye_sta[1]);
    }
+   else if(order == 6){
+    eye_sta[2] = !eye_sta[2];
+    radio.writeAckPayload(pipeNo, &command4, sizeof(eye_sta[2]) );
+    digitalWrite(control2, eye_sta[2]);
+   }
+
+   myServo0.write(servoANG[0] / 10);
+   myServo1.write(servoANG[1] / 10);
+
+   // 更新舵机方位角
 }
